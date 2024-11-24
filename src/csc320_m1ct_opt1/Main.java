7bb4ae916fa0cde397ed6bc83327748b2b80efe7
fadelmcsug <fadel.makhzoum@csuglobal.edu>
2024-11-24 15:37:23 +0100

    Add files via upload

---
+++ src/csc320_m1ct_opt1/Main.java
@@ -0,0 +1,13 @@
+package csc320_m1ct_opt1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Jeremy");
+		System.out.println("Higbee");
+		System.out.println("4894 Tenmile Road");
+		System.out.println("Kokomo");
+		System.out.println("46902");
+	}
+	
+}
